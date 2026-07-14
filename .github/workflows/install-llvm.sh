061b4facea52b47f2d533b083caeecc5ff8bba5f
Asmit Kumar Rai <146473475+asmit27rai@users.noreply.github.com>
2026-07-14 20:04:28 +0530

    Revert "Pinned depend"

--- .github/workflows/install-llvm.sh
+++ .github/workflows/install-llvm.sh
@@ -5,23 +5,25 @@
 . /etc/os-release
 
 sudo apt-get -y install build-essential libelf-dev pkg-config
+wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh
 
 if [ "$VERSION_CODENAME" == "focal" ] || [ "$VERSION_CODENAME" == "bionic" ]; then
-    sudo apt-get install -y clang-12 llvm-12
+    sudo bash /tmp/llvm.sh 12
     for tool in "clang" "llc" "llvm-strip" "opt" "llvm-dis"; do
         sudo rm -f /usr/bin/$tool
         sudo ln -s /usr/bin/$tool-12 /usr/bin/$tool
     done
 elif [ "$VERSION_CODENAME" == "jammy" ]; then
-    sudo apt-get install -y clang-14 llvm-14
+    sudo bash /tmp/llvm.sh 14
     for tool in "clang" "llc" "llvm-strip" "opt" "llvm-dis"; do
         sudo rm -f /usr/bin/$tool
         sudo ln -s /usr/bin/$tool-14 /usr/bin/$tool
     done
-else
-    sudo apt-get install -y clang-19 llvm-19
+else # VERSION_CODENAME == noble
+    sudo  bash /tmp/llvm.sh 19
     for tool in "clang" "llc" "llvm-strip" "opt" "llvm-dis"; do
         sudo rm -f /usr/bin/$tool
         sudo ln -s /usr/bin/$tool-19 /usr/bin/$tool
     done
 fi
+
